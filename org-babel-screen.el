62e5bc032c2a94c8f40cb964aacac606c48de9be
Benjamin Andresen <benny@in-ulm.de>
2009-09-23 12:37:36 +0200

    set default header args for screen

--- org-babel-screen.el
+++ org-babel-screen.el
@@ -36,39 +36,44 @@
 
 (add-to-list 'org-src-lang-modes '("screen" . sh))
 
-(defvar org-babel-screen-terminal "xterm"
-  "Terminal process that should be started when in interactive mode.
-Must support -T for setting title and -e for executing programs.")
+(defvar org-babel-default-header-args:screen
+  '((:results . "silent") (:session . "default") (:cmd . "sh") (:terminal . "xterm"))
+  "Default arguments to use when running screen source blocks.")
 
 (defun org-babel-execute:screen (body params)
-  (message "sending source code block to interactive terminal session...")
+  "Send a block of code via screen to a terminal using org-babel.
+\"default\" session is be used when none is specified."
+  (message "Sending source code block to interactive terminal session...")
   (save-window-excursion
     (let ((socket (org-babel-screen-session-socketname session)))
       (unless socket (progn (org-babel-prep-session:screen session params) (sleep-for 1)))
       (org-babel-screen-session-execute-string session body))))
 
-
 (defun org-babel-prep-session:screen (session params)
   "Prepare SESSION according to the header arguments specified in PARAMS."
   (let* ((socket (org-babel-screen-session-socketname session))
-         (vars (org-babel-ref-variables params)))
-    (apply 'start-process (concat "org-babel: terminal (session " session ")") "*Messages*"
-           org-babel-screen-terminal `("-T" ,(concat "session " session) "-e" "screen"
+         (vars (org-babel-ref-variables params))
+         (cmd (cdr (assoc :cmd params)))
+         (terminal (cdr (assoc :terminal params))))
+    (apply 'start-process (concat "org-babel: terminal (" session ")") "*Messages*"
+           terminal `("-T" ,(concat "org-babel: " session) "-e" "screen"
                                       "-c" "/dev/null" "-mS" ,(concat "org-babel-session-" session)
-                                      "/bin/zsh"))))
+                                      ,cmd))))
 
 ;; helper functions
 
 (defun org-babel-screen-session-execute-string (session body)
+  "If SESSION exist, send BODY to it."
   (let ((socket (org-babel-screen-session-socketname session)))
     (when socket
       (let ((tmpfile (org-babel-screen-session-write-temp-file session body)))
-        (apply 'start-process (concat "screen (session " session ")") "*Messages*"
+        (apply 'start-process (concat "org-babel: screen (" session ")") "*Messages*"
                "screen" `("-S" ,socket "-X" "eval" "msgwait 0"
                                ,(concat "readreg z " tmpfile)
                                "paste z"))))))
 
 (defun org-babel-screen-session-socketname (session)
+  "Check if SESSION exist by parsing output of \"screen -ls\"."
   (let* ((screen-ls (shell-command-to-string "screen -ls"))
          (sockets (remove-if-not
                    '(lambda (x)
@@ -81,6 +86,7 @@ Must support -T for setting title and -e for executing programs.")
     (when match-socket (car (split-string match-socket)))))
 
 (defun org-babel-screen-session-write-temp-file (session body)
+  "Save BODY in a temp file that is named after SESSION."
   (let ((tmpfile (concat "/tmp/screen.org-babel-session-" session)))
     (with-temp-file tmpfile
       (insert body)
