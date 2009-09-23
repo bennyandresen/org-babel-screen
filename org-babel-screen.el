e2a277a7b8ce4f622d895c794e8954bc0f95efe3
Benjamin Andresen <benny@in-ulm.de>
2009-09-23 02:48:58 +0200

    working version with org-babel integration

--- org-babel-screen.el
+++ org-babel-screen.el
@@ -1,8 +1,74 @@
-(defvar org-screen-terminal "xterm"
+;;; org-babel-screen.el --- org-babel support for interactive terminal
+
+;; Copyright (C) 2009 Benjamin Andresen
+
+;; Author: Benjamin Andresen
+;; Keywords: literate programming, interactive shell
+;; Homepage: http://orgmode.org
+;; Version: 0.01
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Org-Babel support for interactive terminals. Mostly shell scripts.
+;; Heavily inspired by 'eev' from Eduardo Ochs
+
+;;; Code:
+(require 'org-babel)
+
+(org-babel-add-interpreter "screen")
+
+(add-to-list 'org-src-lang-modes '("screen" . sh))
+
+(defvar org-babel-screen-terminal "xterm"
   "Terminal process that should be started when in interactive mode.
 Must support -T for setting title and -e for executing programs.")
 
-(defun org-screen-channel-socketname (channel)
+(defun org-babel-execute:screen (body params)
+  (message "sending source code block to interactive terminal session...")
+  (save-window-excursion
+    (let ((socket (org-babel-screen-session-socketname session)))
+      (unless socket (progn (org-babel-prep-session:screen session params) (sleep-for 1)))
+      (org-babel-screen-session-execute-string session body))))
+
+
+(defun org-babel-prep-session:screen (session params)
+  "Prepare SESSION according to the header arguments specified in PARAMS."
+  (let* ((socket (org-babel-screen-session-socketname session))
+         (vars (org-babel-ref-variables params)))
+    (apply 'start-process (concat "org-babel: terminal (session " session ")") "*Messages*"
+           org-babel-screen-terminal `("-T" ,(concat "session " session) "-e" "screen"
+                                      "-c" "/dev/null" "-mS" ,(concat "org-babel-session-" session)
+                                      "/bin/zsh"))))
+
+;; helper functions
+
+(defun org-babel-screen-session-execute-string (session body)
+  (let ((socket (org-babel-screen-session-socketname session)))
+    (when socket
+      (let ((tmpfile (org-babel-screen-session-write-temp-file session body)))
+        (apply 'start-process (concat "screen (session " session ")") "*Messages*"
+               "screen" `("-S" ,socket "-X" "eval" "msgwait 0"
+                               ,(concat "readreg z " tmpfile)
+                               "paste z"))))))
+
+(defun org-babel-screen-session-socketname (session)
   (let* ((screen-ls (shell-command-to-string "screen -ls"))
          (sockets (remove-if-not
                    '(lambda (x)
@@ -10,46 +76,19 @@ Must support -T for setting title and -e for executing programs.")
                    (split-string screen-ls "\n")))
          (match-socket (find-if
                         '(lambda (x)
-                          (string-match (concat "channel-" channel) x))
+                          (string-match (concat "org-babel-session-" session) x))
                         sockets)))
     (when match-socket (car (split-string match-socket)))))
 
-(defun org-screen-channel-start-interactive (channel)
-  (apply 'start-process (concat "terminal (channel " channel ")") "*Messages*"
-         org-screen-terminal `("-T" ,(concat "channel " channel) "-e" "screen"
-         "-c" "/dev/null" "-mS" ,(concat "channel-" channel) "/bin/sh")))
-
-(defun org-screen-channel-write-temp-file (channel input)
-  (let ((tmpfile (concat "/tmp/screen.channel-" channel)))
+(defun org-babel-screen-session-write-temp-file (session body)
+  (let ((tmpfile (concat "/tmp/screen.org-babel-session-" session)))
     (with-temp-file tmpfile
-      (insert input)
-      (insert "\n"))
+      (insert body)
+
+      ;; org-babel has superflous spaces
+      (goto-char (point-min))
+      (delete-matching-lines "^ +$"))
     tmpfile))
 
-(defun org-screen-channel-execute-string (channel input)
-  (let ((socket (org-screen-channel-socketname channel)))
-    (when socket
-      (let ((tmpfile (org-screen-channel-write-temp-file channel input)))
-        (apply 'start-process (concat "screen (channel " channel ")") "*Messages*"
-               "screen" `("-S" ,socket "-X" "eval" "msgwait 0"
-                               ,(concat "readreg z " tmpfile)
-                               "paste z" "slowpaste 0" "msgwait 1"))))))
-
-
-;;; test case from http://angg.twu.net/eev-current/anim/channels.anim.html
-;;; (proof of concept, still needs integration to org-babel)
-(org-screen-channel-start-interactive "A") ;; create
-(org-screen-channel-start-interactive "B") ;; create
-
-(org-screen-channel-execute-string "B"
-"# Listen on port 1234
-netcat -l -p 1234")
-
-(org-screen-channel-execute-string "A"
-"# Send things to port 1234
-{
-  echo hi
-  sleep 1
-  echo bye
-  sleep 1
-} | netcat localhost 1234")
+(provide 'org-babel-screen)
+;;; org-babel-screen.el ends here
